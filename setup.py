bf41e66105a0a8870e3c8bbfd42b78240bb1266e
Shubham Dipt <shubham.dipt@gmail.com>
2021-06-10 12:47:23 +0200

    requirements fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('requirements.txt') as fp:
     install_requires = fp.read()
 
 DESCRIPTION = "A python client for Passbolt."
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 
 with open("README.md", "r") as fh:
